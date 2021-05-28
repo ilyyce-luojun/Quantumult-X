b1ebaed418586b3ae2cdc70578d8b1dd4bde5cd6
ilyyce-luojun <69046763+ilyyce-luojun@users.noreply.github.com>
2021-05-28 14:36:58 +0800

    Update Jd_TaoBao.js

--- JS.conf/Jd_TaoBao.js
+++ JS.conf/Jd_TaoBao.js
@@ -6,8 +6,10 @@ hostname = api.m.jd.com, trade-acs.m.taobao.com
 # 不生效或失效的，需要卸载 taobao 重装，**注意** 不开脚本进 taobao 会失效
 # taobao历史价格在 保障 哪里显示
 
-# 京东比价
-^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+# 京东比价(yichahucha)
+# ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+# 京东比价(JDHelloWorld)
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/JDHelloWorld/jd_price/main/jd_price.js
 # 淘宝比价
 ^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
 ^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
